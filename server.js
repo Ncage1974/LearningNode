9c7822b15de61b289219fe50557b3d0c8e046d12
me <me@example.org>
2015-09-08 18:09:32 -0500

    Using Jade for the view engine

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
+//setup the view engine
+app.set("view engine", "jade");
+
 app.get("/", function(req,res) {
-	res.send("<htmL><body><h2>express is working now</h2></body></html>")
+	//res.send("<htmL><body><h2>express is working now</h2></body></html>")
+	res.render("jade/index", {title: "Express + Jade"});
 });
 
 app.get("/api/users",function(req,res) {
