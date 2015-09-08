2c47f1809878366750bc2fc237ccce3ba25bb79d
me <me@example.org>
2015-09-08 17:01:13 -0500

    Send back an object in express

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function(req,res) {
 	res.send("<htmL><body><h2>express is working now</h2></body></html>")
 });
 
+app.get("/api/users",function(req,res) {
+	res.send({name: "Brent", isValid:true, group: "Admin"});
+});
+
 var server = http.createServer(app);
 
 server.listen(3000);
